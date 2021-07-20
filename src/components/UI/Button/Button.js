57b598f94e67606480a4522fdf8e183145aae938
TriantafyllosFamprikatzis <alkisfab@gmail.com>
2021-07-20 09:05:58 +0300

    Implement button component

---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Button.css'
+
+const Button = () => {
+  return (
+    <button className="button" type="submit">Add User</button>
+  );
+};
+
+export default Button;
--- src/components/UserInput/UserInput.js
+++ src/components/UserInput/UserInput.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
-import Card from '../UI/Card';
+import Card from '../UI/Card/Card';
+import Button from '../UI/Button/Button';
 import './UserInput.css';
 
 const UserInput = (props) => {
@@ -36,15 +37,17 @@ const UserInput = (props) => {
       setAgeIsValid(false);
     }
 
-    const userData = {
-      name: enteredNameValue,
-      age: enteredAgeValue + ' years old',
-      id: Math.random().toString()
-    };
-
-    props.onAddUser(userData);
-    setEnteredNameValue('');
-    setEnteredAgeValue('');
+    if ( enteredAgeValue.trim().length && enteredNameValue.trim().length > 0) {
+      const userData = {
+        name: enteredNameValue,
+        age: enteredAgeValue + ' years old',
+        id: Math.random().toString()
+      };
+  
+      props.onAddUser(userData);
+      setEnteredNameValue('');
+      setEnteredAgeValue('');
+    }
   };
 
   return (
@@ -52,14 +55,14 @@ const UserInput = (props) => {
       <form onSubmit={formSubmitHandler}>
         <div className={nameIsValid ? "form-control" : "form-control invalid"}>
           <label>Username</label>
-          <input type="text" onChange={inputNameHandler}></input>
+          <input type="text" value={enteredNameValue} onChange={inputNameHandler}></input>
         </div>
         <div className={ageIsValid ? "form-control" : "form-control invalid"}>
           <label>Age(Years)</label>
-          <input type="number" onChange={inputAgeHandler} min="1" max="110"></input>
+          <input type="number" value={enteredAgeValue} onChange={inputAgeHandler} min="1" max="110"></input>
         </div>
         <div className="user-controls">
-          <button type="submit">Add User</button>
+          <Button />
         </div>
       </form>
     </Card>
